2dc34f8340840256709003d137e6940068b932de
SwapnilPande <swapnil.pande98@gmail.com>
2025-10-13 19:27:33 +0000

    Deploying to gh-pages from @ SwapnilPande/SwapnilPande.github.io@3a01be0ce98bf7193a59ee532ad5b877405cae1b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -420,10 +420,12 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
-            },},{id: "news-",
-          title: '',
+            },},{id: "news-i-39-m-leaving-every-flavor-robotics-and-am-searching-for-my-next-opportunity",
+          title: 'I&amp;#39;m leaving Every Flavor Robotics and am searching for my next opportunity',
           description: "",
-          section: "News",},{id: "projects-project-1",
+          section: "News",handler: () => {
+              window.location.href = "/news/announcement_3/";
+            },},{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
